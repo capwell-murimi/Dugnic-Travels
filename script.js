c72cc3e66285d4b34fccf9ecd8202f1d41d9afbf
capwell-murimi <capwellmurimi@gmail.com>
2024-04-02 17:49:37 +0300

    rest of the sections added

--- script.js
+++ script.js
@@ -76,3 +76,14 @@ document.addEventListener('DOMContentLoaded', function () {
       }
   });
 });
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
